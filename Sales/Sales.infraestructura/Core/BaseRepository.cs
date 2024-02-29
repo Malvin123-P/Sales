f7f3bc198e254530cabc6cebace0e576d5a89978
MykeLora <maycollora16@gmail.com>
2024-02-29 15:30:56 -0600

    Agregando los metodos Genericos de la interface IBaseRepository

--- Sales/Sales.infraestructura/Core/BaseRepository.cs
+++ Sales/Sales.infraestructura/Core/BaseRepository.cs
@@ -1,12 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Sales.Dominio.Repository;
+using Sales.Infraestructura.Context;
 
 namespace Sales.Infraestructura.Core
 {
-    public abstract class BaseRepository<TEntity> : IBaseRepository where TEntity : class
+    public abstract class BaseRepository<TEntity> : IBaseRepository<TEntity> where TEntity : class
     {
+        private readonly SalesContext context;
+
+        protected BaseRepository(SalesContext context)
+        {
+            this.context = context;
+        }
+
+        public bool Exists(Func<TEntity, bool> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        public List<TEntity> FinAll(Func<TEntity, bool> filter)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<TEntity> GetEntities()
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity GetEntity(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Save(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
