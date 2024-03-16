b28c249e4c0d70d171c2daad021c01feba348b48
randomGuy8-8 <108034246+randomGuy8-8@users.noreply.github.com>
2024-03-16 17:28:09 -0400

    Commit

--- Sales/Sales.Dominio/Entities/Configuracion.cs
+++ Sales/Sales.Dominio/Entities/Configuracion.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+  
 namespace Sales.Dominio.Entities
 {
     public class Configuracion: BaseEntity
