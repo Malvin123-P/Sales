ba8bea542b143fdf6a441bdb845d0cd261914aa8
Malvin Peña <Malvinpm29@Gmail.com>
2024-03-18 02:21:49 -0400

    Api Lista

---
+++ Sales/Sales.AplicacionCasosDEusos/Core/ServiceResult.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sales.AplicacionCasosDEusos.Core
+{
+    public class ServiceResult<TData>
+    {
+        public bool Success { get; set; }
+        public string? Message { get; set; }
+        public TData? Data { get; set; }
+    }
+}
